c6b5f815636d0112a7ea9e77a5dd7a529fb366a1
Ting <san222631@gmail.com>
2024-06-21 14:27:40 +0200

    Part4-1st upload

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import mysql.connector
 from mysql.connector import Error
 from fastapi.staticfiles import StaticFiles
 
+from pydantic import BaseModel
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+import jwt
+from jwt import PyJWTError
+
+
+
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -33,6 +42,246 @@ DB_CONFIG = {
     'database': 'mydatabase',
     'charset': 'utf8'
 }
+
+#使用者註冊
+class registerInfo(BaseModel):
+    name: str
+    email: str
+    password: str
+    
+@app.post("/api/user")
+async def signup(register_Info: registerInfo):
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        cursor = conn.cursor(dictionary=True)
+        register_name = register_Info.name
+        register_email = register_Info.email
+        register_password = register_Info.password
+
+        #確認有沒有一樣的email已經在資料庫
+        cursor.execute("SELECT * FROM member WHERE email = %s", (register_email,))
+        existingUser = cursor.fetchone()
+
+        if (existingUser):
+            cursor.close()
+            conn.close()
+            raise HTTPException(
+                status_code=400,
+                detail={
+                    "error": True,
+                    "message": "email已經註冊過"
+                }
+            )
+           
+
+        register_query = """
+        INSERT INTO member (name, email, hashed_password)
+        VALUES
+        (%s, %s, %s)
+        """
+        cursor.execute(
+            register_query,
+            (register_name, register_email, register_password)
+        )
+        conn.commit()
+        return {"註冊成功": True}
+    except mysql.connector.Error as err:
+        cursor.close()
+        conn.close()
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": True,
+                "message": "MySQL出了問題"
+            }
+        )
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": True,
+                "message": "伺服器內部錯誤"
+            }
+        )
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+		
+
+            
+
+
+
+
+
+#確認使用者的JWT有沒有正確
+#{"data": {"id": 1,"name": "彭彭彭","email": "ply@ply.com"}}
+#要回傳給使用者的資料規定格式
+class verified_user(BaseModel):
+    id: int
+    name: str
+    email: str
+
+@app.get("/api/user/auth")
+async def authenticate(request: Request):
+    #從前端送過來的headers get"Authorization"然後decode
+    auth_header = request.headers.get("Authorization")
+    print(auth_header)
+    if auth_header is None or not auth_header.startswith("Bearer"):
+        raise HTTPException(status_code=401, detail="無授權")
+    extracted_token = auth_header[len("Bearer "):]
+    try:
+        payload = jwt.decode(extracted_token, SECRET_KEY, algorithms=[ALGORITHM])
+    except PyJWTError:
+        payload = None
+        return payload
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+    #資料庫內驗證使用者
+    conn =None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        cursor = conn.cursor(dictionary=True)
+        user_email = payload.get("email")
+        print(user_email)
+        cursor.execute(
+            "SELECT id, name, email FROM member WHERE email = %s",
+            (user_email,)
+        )
+        verified_user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        print(verified_user)
+        if not verified_user:
+            return {"null"}
+        return {"data": verified_user}
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": True,
+                "message": "伺服器內部錯誤"
+            }
+        )
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+    
+
+
+
+
+
+    
+
+#檢查使用者信箱+密碼，確認有的話給一個JWT
+#我指定要符合的Pydantic model
+#如果token_type是bearer，通常代表user要把這個token放在"authorization" header裡面寄request來
+#例如authorization: Bearer <token>
+class Token(BaseModel):
+    token: str
+#使用者要登入時給的資料
+class UserInfo(BaseModel):
+    email: str
+    password: str
+#JWT Secret & Algorithm
+SECRET_KEY = "wehelp"
+ALGORITHM = "HS256"
+#加密&驗證密碼 password hashing context
+#pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+#DEVELOPMENT_MODE = True #正在開發，完成後才變成False
+     
+#檢查使用者信箱+密碼，確認有的話給一個JWT
+#{"email": "ply@ply.com","password": "12345678"}
+#"response_model" 是一個parameter，用在確保這個api送回去的response符合我指定的pydantic model
+@app.put("/api/user/auth", response_model=Token)
+#"user_auth"是一個request body parameter，名字我可自訂
+async def login(user_info: UserInfo):
+    conn = None
+    cursor = None
+    #進資料庫檢查有沒有這個帳號密碼
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        cursor = conn.cursor(dictionary=True)
+        received_email = user_info.email
+        received_password = user_info.password
+        print(user_info.email)
+        cursor.execute(
+            "SELECT id, name, email, hashed_password FROM member WHERE email = %s",
+            (received_email,)
+            )
+        fetched_user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        #verify_password = pwd_context.verify(received_password, fetched_user["hashed_password"])
+        print(fetched_user)
+        #要return None嗎?
+        if not fetched_user:
+            raise HTTPException(
+                status_code=400,
+                detail={
+                "error": True,
+                "message": "找不到這位使用者"                    
+                }
+            )
+        if not received_password == fetched_user["hashed_password"]:
+            raise HTTPException(
+                status_code=400,
+                detail={
+                "error": True,
+                "message": "密碼錯誤"                    
+                }
+            )         
+        
+        #在token加入有效的時間，然後加密
+        original_data = {
+            "id": fetched_user["id"],
+            "name": fetched_user["name"],
+            "email": fetched_user["email"]}
+        print(original_data)
+        data_to_encode = original_data.copy()
+        expire_time = datetime.now(timezone.utc) + timedelta(minutes=30)
+        data_to_encode.update({"exp": expire_time})
+        print(data_to_encode)
+        encoded_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return {"token": encoded_jwt} 
+
+        
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": True,
+                "message": "伺服器內部錯誤"
+            }
+        )
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+        
+    
+
+    
+
 
 #for /api/attractions 取得景點資料
 def fetch_data(page: int, keyword: Optional[str]):
@@ -186,7 +435,7 @@ def fetch_mrts():
         #print("This is ID query", attractionId) #找錯誤
         cursor.execute(query)
         result = cursor.fetchall()
-        print("找到的結果", result)
+        #print("找到的結果", result)
         return result
 
     except Exception as e:
--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 let observer;
 
 document.addEventListener('DOMContentLoaded', function () {
+    fetchUserInfo()
     fetchAttractions(0);
     fetchMRTStations();
 
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('search-button').addEventListener('click', function () {
         const keyword = document.getElementById('search-input').value;
         searchAttractions(keyword);
+        fetchUserInfo()
     });
 
     //MRT 左右滑動的按鍵
@@ -29,6 +31,138 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('mrt-list').scrollBy({ left: 200, behavior: 'smooth' });
     });
 
+
+    //處理登入
+    const modal = document.getElementById('modal');
+    const loginRegister = document.getElementById('login-register');
+    const closeButton = document.getElementById('close-button');
+    const loginButton = document.getElementById('login-button');
+    const errorMessage = document.getElementById('error-message');
+    const login_register = document.getElementById('login-register')
+    const logout = document.getElementById('logout')
+
+  
+    loginRegister.addEventListener('click', () => {
+      modal.style.display = 'block';
+    });
+  
+    closeButton.addEventListener('click', () => {
+      modal.style.display = 'none';
+      errorMessage.textContent = ''; //為什麼每次關閉前要加這個?
+    });
+  
+    window.addEventListener('mousedown', (event) => {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+        errorMessage.textContent = '';
+      }
+    });
+  
+    loginButton.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      fetch('/api/user/auth', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: email, password: password })
+      })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(data => {
+            throw new Error(data.detail.message);
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.token){
+            localStorage.setItem('received_Token', data.token);
+            console.log('Token存進了LocalStorage', data.token);
+            modal.style.display = 'none';
+            errorMessage.textContent = '';
+            location.reload()
+        } else {
+            throw new Error('無效的token response');
+        }
+      })
+      .catch(error => {
+        errorMessage.textContent = error.message;
+        console.error('Error是:', error.message); 
+      });
+    });
+
+
+
+    //註冊    
+    const registerButton = document.getElementById('register');
+    const R_modal = document.getElementById('R-modal'); 
+    const R_closeButton = document.getElementById('R-close-button');
+    const R_errorMessage = document.getElementById('R-error-message');
+    const back_to_login = document.getElementById('back-to-login');
+    const R_form = document.getElementById('R-form');
+
+    registerButton.addEventListener('click', function(){
+        modal.style.display = 'none';
+        R_modal.style.display = 'block';
+    });
+
+    R_closeButton.addEventListener('click', function(){
+        R_modal.style.display = 'none';
+        R_errorMessage.textContent = '';
+    });
+
+    back_to_login.addEventListener('click', function(){
+        R_modal.style.display = 'none';
+        modal.style.display = 'block';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == R_modal) {
+          R_modal.style.display = 'none';
+          R_errorMessage.textContent = '';
+        }
+    });
+
+    R_form.addEventListener('submit', function(event){
+        event.preventDefault(); //避免預設的submission
+        const R_name = document.getElementById('R-name').value;
+        const R_email = document.getElementById('R-email').value;
+        const R_password = document.getElementById('R-password').value;
+
+        fetch('/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({name: R_name, email: R_email, password: R_password})
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(data => {
+                    console.log("收到的response裡面的data:", data)
+                    throw new Error(data.detail.message);
+                });
+              }
+              return response.json();
+        })
+        .then(data => {
+            if (data){
+                console.log(data);
+                R_errorMessage.textContent = '註冊成功';
+            } else {
+                throw new Error('無效的註冊data response');
+            }            
+        })
+        .catch(error => {
+            console.log("抓到的Error:", error);
+            R_errorMessage.textContent = error.message;
+            console.error('註冊Error是:', error.message || error); //這個Error可以看看要改什麼
+        });
+    })
 });
 
 
@@ -101,6 +235,7 @@ function fetchMRTStations() {
                 stationItem.addEventListener('click', function(){
                     document.getElementById('search-input').value = station;
                     searchAttractions(station);
+                    fetchUserInfo();
                 });
 
                 mrtList.appendChild(stationItem);
@@ -132,8 +267,13 @@ function createAttractionCard(attraction) {
     //增加了每個景點獨特的id以及偵測有沒有被click
     card.id = attraction.id;
     card.addEventListener('click', function(){
-        //要記得用backticks``做string interpolation
-        window.location.href = `/attraction/${attraction.id}`;
+        fetchUserInfo().then(function(){
+            //確保檢查過token以後才redirect
+            window.location.href = `/attraction/${attraction.id}`;
+        }).catch(function(){
+            //如果用戶token無效或未登入
+            window.location.href = `/attraction/${attraction.id}`;
+        })
     });
 
     const image = document.createElement('img');
@@ -170,3 +310,71 @@ function handleIntersection(entries, observer) {
     });
 }
 
+
+
+
+//每次重新整理頁面，都檢查一次使用者的TOKEN
+function fetchUserInfo() {
+    const token = localStorage.getItem('received_Token');
+
+    if (!token) {
+        console.error('LocalStorage沒有Token!');
+        renderLogin();
+        //如果沒有token，直接回傳一個promise，讓其他功能也可以使用
+        return Promise.resolve(null);
+    }
+    //這個return很重要，因為有這個才有完整的promise chain，其他功能可以使用驗證以後的promise
+    return fetch('/api/user/auth', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response不ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('從UserApi收到的response', data);
+        //如果使用者token正確，要把收到的data傳給以後的promise chain
+        if (data && data.data){
+            renderLogout();
+            return data.data;
+        } else {
+            //如果使用者token不正確，要把null傳給以後的promise chain
+            renderLogin();
+            return null;
+        }
+    })
+    .catch(error => {
+        //如果使用者token不正確，要把null傳給以後的promise chain
+        console.error('Error fetching user info:', error);
+        renderLogin();
+        return null;
+    });
+}
+
+//render 登出系統
+function renderLogout(){
+    document.getElementById('login-register').style.display = 'none';
+    document.getElementById('logout').style.display = 'block';
+    document.getElementById('login-register').classList.remove('visible');
+    document.getElementById('logout').classList.add('visible');
+}
+//render 登入/註冊
+function renderLogin(){
+    document.getElementById('logout').style.display = 'none';
+    document.getElementById('login-register').style.display = 'block';
+    document.getElementById('logout').classList.remove('visible');
+    document.getElementById('login-register').classList.add('visible');
+}
+
+//登出功能
+document.getElementById('logout').addEventListener('click', function(){
+    localStorage.removeItem('received_Token');
+    //登出後重整頁面
+    location.reload();
+})
